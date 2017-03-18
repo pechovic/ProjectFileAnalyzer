4726b2171b2145d01a94ed8d0fc2068d5f326288
Petr <csnetsource@users.noreply.github.com>
2017-03-18 15:31:45 +0100

    migrated to netcore 1.1

--- ProjectFileAnalyzer.Test/DataBasedTest.cs
+++ ProjectFileAnalyzer.Test/DataBasedTest.cs
@@ -2,7 +2,7 @@ namespace ProjectFileAnalyzer.Test
 {
     public class DataBasedTest
     {
-        protected const string TestSlnFile = "./ProjectFileAnalyzer.Test/test_data/NetDb.sln";
+        protected const string TestSlnFile = "../../../test_data/NetDb.sln";
         protected const string GuidCore = "{6D85E6C7-B860-4ED4-8C3B-C3306BAA18EC}";
         protected const string GuidCommands = "{1CD34AF6-7078-45FD-B888-2990402F924A}";
         protected const string GuidCommandsTests = "{6E46E7CE-12D2-409A-B350-EB805CA6482F}";
